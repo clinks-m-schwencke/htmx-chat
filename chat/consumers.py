eb6c63a3a6597d2df595631e88aa9b6d074abef0
m-schwencke <m-schwencke@clinks.jp>
2026-01-06 10:13:08 +0900

    Message gets added via websockets

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,6 +2,7 @@ import json
 
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.shortcuts import render
 from django.template.loader import get_template
 from .models import Message, Thread
 
@@ -15,7 +16,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.group_name = None
 
     async def connect(self):
+        print("####################")
         print("ws - connect")
+        print("####################")
         # Assign websocket data
         self.thread_id = self.scope["url_route"]["kwargs"]["thread_id"]
         self.group_name = f"chat_{self.thread_id}"
@@ -44,16 +47,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
+        print("####################")
         print("ws - disconnect")
+        print("####################")
         # Disconnect from websocket
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data=None, bytes_data=None):
+        print("####################")
         print("ws - receive", text_data)
+        print("####################")
         # Extract data from JSON
         text_data_json = json.loads(text_data)
 
+        print("####################")
         print("ws - receive: JSON", text_data_json)
+        print("####################")
 
         # Get message
         message = text_data_json["values"]["message"]
@@ -67,26 +76,46 @@ class ChatConsumer(AsyncWebsocketConsumer):
             {
                 "type": "chat_message",
                 "message": message,
+                "username": self.user.username,
             },
         )
 
     async def chat_message(self, event):
+        print("####################")
         print("ws - chat_message", event)
+        print("####################")
+
         # Extract message
         message = event["message"]
+        username = event["username"]
 
         # Send message event to client
-        await self.send(
-            text_data=json.dumps(
-                {
-                    "message": message,
-                }
-            )
+        # await self.send(
+        #     text_data=json.dumps({"message": message, "username": event["username"]})
+        # )
+        payload = get_template("chat/thread_detail.html#message_display").render(
+            context={"message": {"author": username, "body": message}}
         )
+        # payload += (
+        #     "<hx-partial hx-target='#chat-form' hx-swap='innerHTML'>"
+        #     + get_template("chat/thread_detail.html#message_form").render({})
+        #     + "</hx-partial>"
+        # )
+
+        print("####################")
+        print("ws - chat_message payload", payload)
+
+        response = json.dumps({"payload": payload})
+        print("####################")
+        print("ws - chat_message response", response)
+
+        await self.send(text_data=response)
 
     @sync_to_async
     def save_message(self, thread_id, user, message):
+        print("####################")
         print("ws - save_message", user, message)
+        print("####################")
         thread = Thread.objects.get(pk=thread_id)
         message = Message.objects.create(thread_id=thread, author=user, body=message)
         message.save()
